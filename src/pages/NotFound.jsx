1b28d96df55794d435b01f1db95b7af1d17765e4
mdselimme <mdselimdev@gmail.com>
2025-02-06 12:57:24 +0600

    404 not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,26 @@
-
+import { Button, Typography, Box } from "@mui/material";
+import { Link } from "react-router";
 
 const NotFound = () => {
+
     return (
-        <div>
-            <h1>Not found</h1>
-        </div>
+        <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="100vh" bgcolor="grey.100">
+            <Typography variant="h1" color="textPrimary" fontWeight="bold">
+                404
+            </Typography>
+            <Typography variant="h5" color="textSecondary" mt={2}>
+                Page Not Found
+            </Typography>
+            <Link to={'/'}>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    sx={{ mt: 2 }}
+                >
+                    Go to Home
+                </Button>
+            </Link>
+        </Box>
     );
 };
 
